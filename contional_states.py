66cb17168f639a093f0c37a461ad657a7adc52de
MUTHARASU A <mutharasu39@gmail.com>
2023-11-03 11:19:01 +0530

    contional inserted

--- contional_states.py
+++ contional_states.py
@@ -1,5 +1,16 @@
 #To check the condition in python using if contional statements 
 
 valuess = True
-if valuess =='True':
-    print(' the value is true')
+if valuess ==True:
+    print(' the value is true') 
+
+#if else statements
+a=40
+b=20
+
+if a > b:
+    print('a is greater than b')
+elif b > a:
+    print('b is greater than a')
+else: 
+  print("a and b is equal")  
