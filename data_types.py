cb611e83299f00033141b8a376c9282f6b948325
MutharasuArchunan13 <mutharasu39@gmail.com>
2023-11-21 09:03:19 +0530

    started python cross-course book

---
+++ data_types.py
@@ -0,0 +1,13 @@
+#integer here python don't require to mention the datatype
+
+print(3+4)
+print(3-4)
+print(3 * 4)
+print(3/2)
+print(3 % 2)
+print(3 ** 2)
+
+fvrt_num = 2
+print('my favorite number is : '+str(fvrt_num))
+
+print('my favorite num is  : ',fvrt_num)
---
+++ string_methods.py
@@ -0,0 +1,13 @@
+#series of characters as known as strings
+message = "The inner beauty is more important than outer imporants"
+# To make each word as capitalize
+print(message.title())
+
+#remove the white_spaces
+messag = ' hey hello dude    '
+print(messag.strip())
+
+# To make characters upper and lower
+print(messag.lower())
+print(messag.upper())
+
